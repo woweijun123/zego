8d9b42c1c68358d2363396249caa32c74dc49fbc
weichengjun <1054487195@qq.com>
2026-04-10 18:33:29 +0800

    即构SDK封装

--- src/ZegoRtcApiClientFactory.php
+++ src/ZegoRtcApiClientFactory.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Zego;
 
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Di\Container;
 
 class ZegoRtcApiClientFactory
 {
-    public function __invoke(ConfigInterface $config): ZegoRtcApiClient
+    public function __invoke(Container $container): ZegoRtcApiClient
     {
+        $config = $container->get(ConfigInterface::class);
         return new ZegoRtcApiClient(
             (int)$config->get('zego.app_id', 0),
             (string)$config->get('zego.secret', ''),
